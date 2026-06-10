446a727baf8cb7bdd672f7331feed4e9b06f7551
Tal Feldman <95024439+talz0rs@users.noreply.github.com>
2026-06-10 10:18:53 +0000

    feat: add celery worker with a sample task

---
+++ src/worker.py
@@ -0,0 +1,13 @@
+from celery import Celery
+
+
+celery_app = Celery(
+    "worker",
+    broker="redis://localhost:6379/0",
+    backend="redis://localhost:6379/0",
+)
+
+
+@celery_app.task
+def add(x, y):
+    return x + y
